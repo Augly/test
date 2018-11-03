a5e3f74a1fa0b69309b0771f96001154003d6546
zero <1137774080@qq.com>
2018-11-03 11:38:41 +0800

    修复bug二维码显示

--- text/pages/indextwo/indextwo.js
+++ text/pages/indextwo/indextwo.js
@@ -8,7 +8,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    savatext:'保存图片',
+    savatext: '保存图片',
     mainHeight: 0,
     selectIndex: 0,
     isShow: true,
@@ -29,7 +29,7 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
   select(e) {
@@ -44,7 +44,7 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
     const that = this;
     util.rem(463, (res) => {
       this.setData({
@@ -52,7 +52,7 @@ Page({
       })
     })
     wx.getSystemInfo({
-      success: function (res) {
+      success: function(res) {
         that.setData({
           systemInfo: res,
           rem: res.screenWidth / 750
@@ -73,39 +73,39 @@ Page({
     wx.showLoading({
       title: '正在计算',
       mask: true,
-      success: function (res) { },
-      fail: function (res) { },
-      complete: function (res) { },
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
     })
     let result = []
     if (this.data.result[0] == '会计/助理会计/出纳' && this.data.result[1] == '1-3年') {
       result = [{
-        title: '初级会计职称',
-        main: '会计职称是衡量个人会计业务水平高低的标准，会计职称越高，表明你会计业务水平越高。现有会计职称：初级、中级和高级，证书全国范围内有效。'
-      }, {
-        title: 'ACCA',
-        main: 'ACCA在国内称为"国际注册会计师"，被认为是"国际财会界的通行证"。许多国家立法许可ACCA会员从事审计、投资顾问和破产执行工作。'
+          title: '初级会计职称',
+          main: '会计职称是衡量个人会计业务水平高低的标准，会计职称越高，表明你会计业务水平越高。现有会计职称：初级、中级和高级，证书全国范围内有效。'
+        }, {
+          title: 'ACCA',
+          main: 'ACCA在国内称为"国际注册会计师"，被认为是"国际财会界的通行证"。许多国家立法许可ACCA会员从事审计、投资顾问和破产执行工作。'
 
-      },
-      {
-        title: 'CMA',
-        main: 'CMA即美国注册管理会计师，是美国管理会计师协会（IMA）旗下的注册管理会计师认证（CMA认证），同时被誉为财会界的Mini-MBA及美国财会领域的三大黄金认证之一，在全球范围内被企业财务高管所认可。'
+        },
+        {
+          title: 'CMA',
+          main: 'CMA即美国注册管理会计师，是美国管理会计师协会（IMA）旗下的注册管理会计师认证（CMA认证），同时被誉为财会界的Mini-MBA及美国财会领域的三大黄金认证之一，在全球范围内被企业财务高管所认可。'
 
-      }
+        }
 
       ]
     } else if (this.data.result[0] == '审计员/主管' && this.data.result[1] == '1-3年') {
       result = [{
-        title: '中级会计职称',
-        main: '会计职称是衡量个人会计业务水平高低的标准，会计职称越高，表明你会计业务水平越高。现有会计职称：初级、中级和高级，证书全国范围内有效。'
-      }, {
-        title: '审计师',
-        main: '审计师是专门从事检查并进一步证实公司会计账目和报告的正确性、 合理性和可接受性的专业人员'
-      },
-      {
-        title: 'CPA',
-        main: '注册会计师缩写，从事社会审计/中介审计/独立审计等专业人士优选证书，CPA为中国唯一官方认可的注册会计师资质，唯一拥有签字权的执业资质。'
-      }
+          title: '中级会计职称',
+          main: '会计职称是衡量个人会计业务水平高低的标准，会计职称越高，表明你会计业务水平越高。现有会计职称：初级、中级和高级，证书全国范围内有效。'
+        }, {
+          title: '审计师',
+          main: '审计师是专门从事检查并进一步证实公司会计账目和报告的正确性、 合理性和可接受性的专业人员'
+        },
+        {
+          title: 'CPA',
+          main: '注册会计师缩写，从事社会审计/中介审计/独立审计等专业人士优选证书，CPA为中国唯一官方认可的注册会计师资质，唯一拥有签字权的执业资质。'
+        }
 
       ]
     } else if (this.data.result[0] == '财务主管/经理' && this.data.result[1] == '1-3年') {
@@ -192,17 +192,17 @@ Page({
       }]
     } else if (this.data.result[0] == '审计员/主管' && this.data.result[1] == '3-5年') {
       result = [{
-        title: 'USCPA',
-        main: 'USCPA是IFRS时代世界通行证。可在全球范围内与多国的注册会计师体系完成互认。（加拿大、英国、中国香港、新西兰、澳大利亚、爱尔兰等）'
-      }, {
-        title: '审计师',
-        main: '审计师是专门从事检查并进一步证实公司会计账目和报告的正确性、 合理性和可接受性的专业人员 '
-      },
-      {
-        title: 'CPA',
-        main: '注册会计师缩写，从事社会审计/中介审计/独立审计等专业人士优选证书，CPA为中国唯一官方认可的注册会计师资质，唯一拥有签字权的执业资质。'
+          title: 'USCPA',
+          main: 'USCPA是IFRS时代世界通行证。可在全球范围内与多国的注册会计师体系完成互认。（加拿大、英国、中国香港、新西兰、澳大利亚、爱尔兰等）'
+        }, {
+          title: '审计师',
+          main: '审计师是专门从事检查并进一步证实公司会计账目和报告的正确性、 合理性和可接受性的专业人员 '
+        },
+        {
+          title: 'CPA',
+          main: '注册会计师缩写，从事社会审计/中介审计/独立审计等专业人士优选证书，CPA为中国唯一官方认可的注册会计师资质，唯一拥有签字权的执业资质。'
 
-      }
+        }
 
       ]
     } else if (this.data.result[0] == '财务主管/经理' && this.data.result[1] == '3-5年') {
@@ -295,18 +295,18 @@ Page({
       }]
     } else if (this.data.result[0] == '审计员/主管' && this.data.result[1] == '5-7年') {
       result = [{
-        title: '经济师',
-        main: '经济师，是我国职称之一，设两个级别：经济专业初级资格、经济专业中级资格。参加考试并成绩合格者，获得相应级别的专业技术资格，由人事部统一发放合格证书。'
+          title: '经济师',
+          main: '经济师，是我国职称之一，设两个级别：经济专业初级资格、经济专业中级资格。参加考试并成绩合格者，获得相应级别的专业技术资格，由人事部统一发放合格证书。'
 
-      }, {
-        title: '审计师',
-        main: '审计师是专门从事检查并进一步证实公司会计账目和报告的正确性、合理性和可接受性的专业人员'
+        }, {
+          title: '审计师',
+          main: '审计师是专门从事检查并进一步证实公司会计账目和报告的正确性、合理性和可接受性的专业人员'
 
-      },
-      {
-        title: 'CPA',
-        main: '注册会计师缩写，从事社会审计/中介审计/独立审计等专业人士优选证书，CPA为中国唯一官方认可的注册会计师资质，唯一拥有签字权的执业资质。'
-      }
+        },
+        {
+          title: 'CPA',
+          main: '注册会计师缩写，从事社会审计/中介审计/独立审计等专业人士优选证书，CPA为中国唯一官方认可的注册会计师资质，唯一拥有签字权的执业资质。'
+        }
 
       ]
     } else if (this.data.result[0] == '财务主管/经理' && this.data.result[1] == '5-7年') {
@@ -339,8 +339,8 @@ Page({
         main: 'CMA即美国注册管理会计师，是美国管理会计师协会（IMA）旗下的注册管理会计师认证（CMA认证），同时被誉为财会界的Mini-MBA及美国财会领域的三大黄金认证之一，在全球范围内被企业财务高管所认可。'
 
       }, {
-          title: 'USCPA',
-          main: 'USCPA是IFRS时代世界通行证。可在全球范围内与多国的注册会计师体系完成互认。（加拿大、英国、中国香港、新西兰、澳大利亚、爱尔兰等）'
+        title: 'USCPA',
+        main: 'USCPA是IFRS时代世界通行证。可在全球范围内与多国的注册会计师体系完成互认。（加拿大、英国、中国香港、新西兰、澳大利亚、爱尔兰等）'
       }, {
         title: 'CPA',
         main: '注册会计师缩写，从事社会审计/中介审计/独立审计等专业人士优选证书，CPA为中国唯一官方认可的注册会计师资质，唯一拥有签字权的执业资质。'
@@ -410,19 +410,19 @@ Page({
       }]
     } else if (this.data.result[0] == '审计员/主管' && this.data.result[1] == '7-10年') {
       result = [{
-        title: '经济师',
-        main: '经济师，是我国职称之一，设两个级别：经济专业初级资格、经济专业中级资格。参加考试并成绩合格者，获得相应级别的专业技术资格，由人事部统一发放合格证书。'
+          title: '经济师',
+          main: '经济师，是我国职称之一，设两个级别：经济专业初级资格、经济专业中级资格。参加考试并成绩合格者，获得相应级别的专业技术资格，由人事部统一发放合格证书。'
 
-      }, {
-        title: 'CMA',
-        main: 'CMA即美国注册管理会计师，是美国管理会计师协会（IMA）旗下的注册管理会计师认证（CMA认证），同时被誉为财会界的Mini-MBA及美国财会领域的三大黄金认证之一，在全球范围内被企业财务高管所认可。'
+        }, {
+          title: 'CMA',
+          main: 'CMA即美国注册管理会计师，是美国管理会计师协会（IMA）旗下的注册管理会计师认证（CMA认证），同时被誉为财会界的Mini-MBA及美国财会领域的三大黄金认证之一，在全球范围内被企业财务高管所认可。'
 
-      },
-      {
-        title: 'CPA',
-        main: '注册会计师缩写，从事社会审计/中介审计/独立审计等专业人士优选证书，CPA为中国唯一官方认可的注册会计师资质，唯一拥有签字权的执业资质。'
+        },
+        {
+          title: 'CPA',
+          main: '注册会计师缩写，从事社会审计/中介审计/独立审计等专业人士优选证书，CPA为中国唯一官方认可的注册会计师资质，唯一拥有签字权的执业资质。'
 
-      }
+        }
 
       ]
     } else if (this.data.result[0] == '财务主管/经理' && this.data.result[1] == '7-10年') {
@@ -495,15 +495,15 @@ Page({
       }]
     } else if (this.data.result[0] == '审计员/主管' && this.data.result[1] == '10-15年') {
       result = [{
-        title: 'USCPA',
-        main: 'USCPA是IFRS时代世界通行证。可在全球范围内与多国的注册会计师体系完成互认。（加拿大、英国、中国香港、新西兰、澳大利亚、爱尔兰等）'
+          title: 'USCPA',
+          main: 'USCPA是IFRS时代世界通行证。可在全球范围内与多国的注册会计师体系完成互认。（加拿大、英国、中国香港、新西兰、澳大利亚、爱尔兰等）'
 
-      },
-      {
-        title: 'CPA',
-        main: '注册会计师缩写，从事社会审计/中介审计/独立审计等专业人士优选证书，CPA为中国唯一官方认可的注册会计师资质，唯一拥有签字权的执业资质。'
+        },
+        {
+          title: 'CPA',
+          main: '注册会计师缩写，从事社会审计/中介审计/独立审计等专业人士优选证书，CPA为中国唯一官方认可的注册会计师资质，唯一拥有签字权的执业资质。'
 
-      }
+        }
 
       ]
     } else if (this.data.result[0] == '财务主管/经理' && this.data.result[1] == '10-15年') {
@@ -587,14 +587,14 @@ Page({
       }]
     } else if (this.data.result[0] == '审计员/主管' && this.data.result[1] == '15年以上') {
       result = [{
-        title: '税务师',
-        main: '从事税务代理的专门人员，税务师资格的取得实行考试和认定制度，证书登记服务的具体工作由全国税务师行业协会负责。'
+          title: '税务师',
+          main: '从事税务代理的专门人员，税务师资格的取得实行考试和认定制度，证书登记服务的具体工作由全国税务师行业协会负责。'
 
-      },
-      {
-        title: 'CPA',
-        main: '注册会计师缩写，从事社会审计/中介审计/独立审计等专业人士优选证书，CPA为中国唯一官方认可的注册会计师资质，唯一拥有签字权的执业资质。'
-      }
+        },
+        {
+          title: 'CPA',
+          main: '注册会计师缩写，从事社会审计/中介审计/独立审计等专业人士优选证书，CPA为中国唯一官方认可的注册会计师资质，唯一拥有签字权的执业资质。'
+        }
 
       ]
     } else if (this.data.result[0] == '财务主管/经理' && this.data.result[1] == '15年以上') {
@@ -665,8 +665,8 @@ Page({
         main: 'CMA即美国注册管理会计师，是美国管理会计师协会（IMA）旗下的注册管理会计师认证（CMA认证），同时被誉为财会界的Mini-MBA及美国财会领域的三大黄金认证之一，在全球范围内被企业财务高管所认可。'
 
       }, {
-          title: 'CFP',
-          main: 'CFP国际金融理财师简称，全世界公认的金融理财行业权威等级证书。 广泛授予金融理财领域内的专业人员，包括理财经理、基金经理、财务总监、投资顾问、投资银行家、理财顾问等。'
+        title: 'CFP',
+        main: 'CFP国际金融理财师简称，全世界公认的金融理财行业权威等级证书。 广泛授予金融理财领域内的专业人员，包括理财经理、基金经理、财务总监、投资顾问、投资银行家、理财顾问等。'
 
       }]
     } else if (this.data.result[0] == '融资/外汇专员' && this.data.result[1] == '15年以上') {
@@ -694,6 +694,7 @@ Page({
     var that = this
     let imglist = []
     let n = 0
+
     function up() {
       wx.downloadFile({
         url: list[n],
@@ -748,63 +749,59 @@ Page({
       isShow: false
     })
   },
-  erweima(){
+  erweima() {
     wx.previewImage({
-      current: '/utils/images/20181103113050.jpg', // 当前显示图片的http链接
-      urls: ['/utils/images/20181103113050.jpg'] // 需要预览的图片http链接列表
+      current: 'http://www.kiss-me.top/20181103113050.jpg', // 当前显示图片的http链接
+      urls: ['http://www.kiss-me.top/20181103113050.jpg'] // 需要预览的图片http链接列表
     })
   },
-  save(){
-    var that=this
-    if (that.data.savatext=='重新测试'){
+  save() {
+    var that = this
+    if (that.data.savatext == '重新测试') {
       wx.reLaunch({
         url: '/pages/index/index'
       })
-    }else{
-
-
-    wx.showLoading({
-      title: '正在保存...',
-      mask: true,
-      success: function(res) {},
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-    setTimeout(function () {
-      wx.canvasToTempFilePath({
-        x: 0,
-        y: 0,
-        width: that.data.systemInfo.screenWidth,
-        height: that.data.systemInfo.screenHeight,
-        canvasId: 'myCanvas',
-        success: function (res) {
-          app.globalData.imgurl = res.tempFilePath
-          wx.saveImageToPhotosAlbum({
-            filePath: res.tempFilePath,
-            success(res) {
-              wx.hideLoading()
-              wx.showToast({
-                title: '保存成功!',
-                icon: '',
-                image: '',
-                duration: 2000,
-                mask: true,
-                success: function (res) { },
-                fail: function (res) { },
-                complete: function (res) { },
-              })
-              that.setData({
-                savatext:'重新测试'
-              })
-            },
-            fail(res) {
-            },
-            complete(res) {
-            }
-          })
-        }
+    } else {
+      wx.showLoading({
+        title: '正在保存...',
+        mask: true,
+        success: function(res) {},
+        fail: function(res) {},
+        complete: function(res) {},
       })
-    }, 1500)
+      setTimeout(function() {
+        wx.canvasToTempFilePath({
+          x: 0,
+          y: 0,
+          width: that.data.systemInfo.screenWidth,
+          height: that.data.systemInfo.screenHeight,
+          canvasId: 'myCanvas',
+          success: function(res) {
+            app.globalData.imgurl = res.tempFilePath
+            wx.saveImageToPhotosAlbum({
+              filePath: res.tempFilePath,
+              success(res) {
+                wx.hideLoading()
+                wx.showToast({
+                  title: '保存成功!',
+                  icon: '',
+                  image: '',
+                  duration: 2000,
+                  mask: true,
+                  success: function(res) {},
+                  fail: function(res) {},
+                  complete: function(res) {},
+                })
+                that.setData({
+                  savatext: '重新测试'
+                })
+              },
+              fail(res) {},
+              complete(res) {}
+            })
+          }
+        })
+      }, 1500)
     }
   },
   drawtitle(ctx, textmain, t, that) {
@@ -867,42 +864,42 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
     return {
       title: '一键分析本命证书',
       path: '/page/index/index'
